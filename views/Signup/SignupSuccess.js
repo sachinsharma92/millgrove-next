3692de68f3c33ccfa764c053505e42f758d2a86f
sachinsharma92 <sachindesigner2018@gmail.com>
2022-12-07 12:26:56 +0530

    FIX: some major things

--- views/Signup/SignupSuccess.js
+++ views/Signup/SignupSuccess.js
@@ -5,21 +5,21 @@ import Header from '../../components/Header';
 
 
 const SignupSuccess = () => {
-    return (
-        <Layout>
-            <div className={styles.successMsgWrapper}>
-                <Header />
-                <div>
-                    <h2 className={styles.title2}>Thank You <span className={styles.smallTxt}>for</span> Registering</h2>
-                    <div className={styles.successDescription}>
-                        <p>A member of our team will be in touch shortly via phone to discuss your interest and requirements.</p>
-                    </div>
-                </div>
-            </div>
+	return (
+		<Layout>
+			<div className={styles.successMsgWrapper}>
+				<Header />
+				<div>
+					<h2 className={styles.title2}>Thank You <span className={styles.smallTxt}>for</span> Registering</h2>
+					<div className={styles.successDescription}>
+						<p>A member of our team will be in touch shortly via phone to discuss your interest and requirements.</p>
+					</div>
+				</div>
+			</div>
 
-        </Layout>
+		</Layout>
 
-    )
+	)
 }
 
 export default SignupSuccess
